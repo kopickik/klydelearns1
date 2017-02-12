802f57817e397f06688b6d55597d49bd09a03eda
Kyle Kopicki <kyle.kopicki@gmail.com>
2017-02-12 13:45:18 -0500

    catchup

---
+++ promise-it-wont-hurt/ex2.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var promise = new Promise(function (fulfill, reject) {
+  setTimeout(function() {
+    fulfill('FULFILLED!')
+  }, 400)
+});
+
+promise.then(console.log)
+
+// Your solution here
