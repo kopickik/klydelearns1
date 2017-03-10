93fba691619ac714ab4a9fe5ae880d19261ce6a7
Kyle Kopicki <kkopick1@ford.com>
2017-03-10 11:33:31 -0500

    catch, fulfill and reject shortcuts.

---
+++ promise-it-wont-hurt/ex6.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var message,
+    promise;
+
+function randomBytes (n) {
+    return (Math.random() * Math.pow(256, n) | 0).toString(16);
+}
+
+message = 'A fatal exception ' + randomBytes(1) + ' has occurred at ' +
+          randomBytes(2) + ':' + randomBytes(4) + '.\nYour system will be ' +
+          'terminated in 3 seconds.';
+
+promise = Promise.reject(new Error(message));
+
+promise.catch(function (err) {
+    var i = 3;
+
+    process.stderr.write(err.message);
+
+    setTimeout(function boom() {
+        process.stderr.write('\rwill be terminated in ' + (--i) + ' seconds.');
+        if (!i) {
+            process.stderr.write('\n.... . . . boom . . . ....\n');
+        } else {
+            setTimeout(boom, 2000);
+        }
+    }, 1000);
+});
