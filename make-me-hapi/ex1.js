f960e431b9976132b94fedfa36f4504d64866a7a
Kyle Kopicki <kyle.kopicki@gmail.com>
2017-08-07 14:42:03 -0400

    makemehapi exercises 1 and 2

---
+++ make-me-hapi/ex1.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const Hapi = require('hapi')
+
+const server = new Hapi.Server();
+
+server.connection({
+  port: Number(process.argv[2]) || 8080,
+  host: 'localhost'
+})
+
+server.route({
+  path: '/',
+  method: 'GET',
+  handler: (request, reply) => { reply('Hello hapi.')}
+})
+
+server.route({
+  path: '/{name}',
+  method: 'GET',
+  handler: myFunction
+})
+
+function myFunction (request, reply) {
+  reply(`Hello ${request.params.name}`)
+}
+
+server.start(function (err) {
+  if (err) { throw err }
+  console.log(`Server running at: ${server.info.uri}`)
+})
