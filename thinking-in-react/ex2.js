93724974d1bfe4cdd9200897311319ea5b2abf70
Kyle Kopicki <kyle.kopicki@gmail.com>
2017-08-07 14:30:55 -0400

    :confused: ...

---
+++ thinking-in-react/ex2.js
@@ -0,0 +1,12 @@
+'use strict'
+
+import React from 'react'
+const createReactClass = require('create-react-class')
+
+export const SearchBar = createReactClass({
+  render() {
+    return (
+      <div></div>
+    )
+  }
+})
