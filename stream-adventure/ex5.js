f9236594d862e470811dafd07b0e377ebd05a07b
Kyle Kopicki <kyle.kopicki@gmail.com>
2018-01-06 18:38:05 -0500

    ex5 stream-adventure - ternary decisions

---
+++ stream-adventure/ex5.js
@@ -0,0 +1,16 @@
+const through = require('through2')
+const split = require('split2')
+
+let lineNumber = 1
+
+process.stdin
+  .pipe(split())
+  .pipe(through(function (line, _, next) {
+    this.push(lineNumber % 2 === 0
+      ? line.toString().toUpperCase()+'\n'
+      : line.toString().toLowerCase()+'\n'
+    )
+    lineNumber++
+    next()
+  }))
+  .pipe(process.stdout)
