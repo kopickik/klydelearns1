9bb1decfe5235c23d748c340e83ed5413876c9f3
Kyle Kopicki <kyle.kopicki@gm.com>
2018-10-04 12:26:52 -0400

    fixing reduce-boxarts.js to not just return the last item in the array

--- rxjs-examples/src/reduce-boxarts.js
+++ rxjs-examples/src/reduce-boxarts.js
@@ -6,17 +6,18 @@ const boxarts = [
     { width: 300, height: 200, url: "http://cdn-0.nflximg.com/images/2891/Fracture300.jpg" },
     { width: 425, height: 150, url: "http://cdn-0.nflximg.com/images/2891/Fracture425.jpg" }
 ]
-
+// _.reduce boils an array down to a single value
+// in this example, the boxart with the largest width x height
 function reduceBoxarts(boxarts) {
     return _
-        .map(boxarts, (boxart) => boxart.url)
-        .reduce((acc, curr) => {
-            if (acc.width * acc.height > curr.width * curr.height) {
-                return acc
+        // .map(boxarts, (boxart) => boxart.url)
+        .reduce(boxarts, (prev, curr) => {
+            if (prev.width * prev.height > curr.width * curr.height) {
+                return prev
             } else {
                 return curr
             }
-        })
+        }).url
 }
 
 console.log(reduceBoxarts(boxarts));
